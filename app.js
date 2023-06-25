dbcbd4c813635d50f3c3d94134b35725583e5b97
olehmartynenko <martynenkooleg77@gmail.com>
2023-06-25 23:36:12 +0300

    add error handling

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import errorHandler, { invalidPathHandler } from './middlewares/errorHandler.js'
 import dotenv from 'dotenv'
 dotenv.config()
 const PORT = process.env.PORT || 3000
@@ -9,6 +10,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use(invalidPathHandler)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,34 @@
+class BaseError extends Error {
+  constructor(message, statusCode, name) {
+    super(message)
+    this.statusCode = statusCode
+    this.name = name
+  }
+}
+
+export class NotFoundError extends BaseError {
+  constructor(message = 'Resource not found') {
+    super(message, 404, 'NotFoundError')
+  }
+}
+
+export const invalidPathHandler = (req, res, next) => {
+  next(new NotFoundError('Path not found'))
+}
+
+export default function errorHandler(err, req, res, next) {
+  if (err instanceof BaseError) {
+    return res.status(err.statusCode).json({
+      status: err.statusCode,
+      name: err.name,
+      message: err.message,
+    })
+  } else {
+    console.error(err)
+    return res.status(500).json({
+      status: 500,
+      name: 'InternalServerError',
+      message: 'An unexpected error occurred',
+    })
+  }
+}
