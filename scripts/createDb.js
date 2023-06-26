c9f7ee0cc3a35e3628b6ff5aed724ed247082016
olehmartynenko <martynenkooleg77@gmail.com>
2023-06-26 20:17:44 +0300

    add create and seed db

---
+++ scripts/createDb.js
@@ -0,0 +1,24 @@
+import db from '../models/index.js'
+const { Sequelize, sequelize } = db
+import config from '../config/db.js'
+
+const createDb = async () => {
+  const rootSequelize = new Sequelize({
+    host: config.host,
+    password: config.password,
+    username: config.username,
+    dialect: config.dialect,
+  })
+  try {
+    await rootSequelize.query(`CREATE DATABASE ${config.database}`)
+    console.log('Database created')
+  } catch (error) {
+    console.log(error)
+  } finally {
+    await rootSequelize.close()
+    await sequelize.sync()
+    await sequelize.close()
+  }
+}
+
+createDb()
