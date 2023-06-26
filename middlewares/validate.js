dc4df372fe53d39e9bbbd50fd09a0f19e573f3b9
olehmartynenko <martynenkooleg77@gmail.com>
2023-06-26 18:27:19 +0300

    add more functionality to validation

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,8 +1,8 @@
 import { ValidationError } from './errorHandler.js'
 
-export default function validate(schema) {
+export default function validate(schema, property = 'body') {
   return async (req, res, next) => {
-    const { error } = schema.validate(req.body, { abortEarly: false })
+    const { error } = schema.validate(req[property], { abortEarly: false })
     if (error) {
       const errorMessages = error.details
         .map((err) => err.message)
