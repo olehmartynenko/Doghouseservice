0284fd352de45381cf595856a2315e74aa893024
olehmartynenko <martynenkooleg77@gmail.com>
2023-06-26 18:47:27 +0300

    add whole query validation

--- routes/dogRoutes.js
+++ routes/dogRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { createDog, getDogs } from '../controllers/dogController.js'
 import { dogSchema } from '../schemas/dogSchema.js'
-import { sortSchema } from '../schemas/sortSchema.js'
+import { querySchema } from '../schemas/querySchema.js'
 import validate from '../middlewares/validate.js'
 import paginate from '../middlewares/paginate.js'
 import sort from '../middlewares/sort.js'
@@ -10,6 +10,6 @@ const router = Router()
 
 router.post('/dog', validate(dogSchema), createDog)
 
-router.get('/dogs', validate(sortSchema, 'query'), sort, paginate, getDogs)
+router.get('/dogs', validate(querySchema, 'query'), sort, paginate, getDogs)
 
 export default router
--- schemas/querySchema.js
+++ schemas/querySchema.js
@@ -3,7 +3,7 @@ import Joi from 'joi'
 const sortingOptions = ['name', 'color', 'tail_length', 'weight']
 const orderOptions = ['asc', 'desc']
 
-export const sortSchema = Joi.object({
+export const querySchema = Joi.object({
   attribute: Joi.string()
     .empty('')
     .valid(...sortingOptions)
@@ -16,4 +16,12 @@ export const sortSchema = Joi.object({
     .messages({
       'any.only': `Order must be one of ${orderOptions.join(', ')}`,
     }),
-}).unknown()
+  pageNumber: Joi.number().empty('').integer().min(1).messages({
+    'number.min': 'Page number must be greater than or equal to 1',
+  }),
+  pageSize: Joi.number().empty('').integer().min(1).messages({
+    'number.min': 'Page size must be greater than or equal to 1',
+  }),
+}).messages({
+  'object.unknown': 'Query parameter not allowed',
+})
